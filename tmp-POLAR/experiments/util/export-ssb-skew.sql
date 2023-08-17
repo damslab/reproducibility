f38b9a5bbae1c2158c0cbba2ee5cb2c7ea912968
David Justen <mail@davidjusten.de>
2023-08-17 16:12:38 +0200

    [POLAR] Fix

--- tmp-POLAR/experiments/util/export-ssb-skew.sql
+++ tmp-POLAR/experiments/util/export-ssb-skew.sql
@@ -3,5 +3,5 @@ COPY customer TO 'PATHVAR/data/ssb-skew/customer.tbl' ( FORMAT CSV, DELIMITER '|
 COPY part TO 'PATHVAR/data/ssb-skew/part.tbl' ( FORMAT CSV, DELIMITER '|' );
 COPY supplier TO 'PATHVAR/data/ssb-skew/supplier.tbl' ( FORMAT CSV, DELIMITER '|' );
 COPY date TO 'PATHVAR/data/ssb-skew/date.tbl' ( FORMAT CSV, DELIMITER '|' );
-COPY locust 'PATHVAR/data/ssb-skew/locust.tbl' ( FORMAT CSV, DELIMITER '|' );
-COPY lopart 'PATHVAR/data/ssb-skew/lopart.tbl' ( FORMAT CSV, DELIMITER '|' );
+COPY locust TO 'PATHVAR/data/ssb-skew/locust.tbl' ( FORMAT CSV, DELIMITER '|' );
+COPY lopart TO 'PATHVAR/data/ssb-skew/lopart.tbl' ( FORMAT CSV, DELIMITER '|' );
