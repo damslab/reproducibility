49681136017e82a2b367082c12a29a9b5772c45b
David Justen <mail@davidjusten.de>
2023-08-30 14:23:07 +0200

    [POLAR] Fix SkinnerMT installation process

--- tmp-POLAR/installDependencies.sh
+++ tmp-POLAR/installDependencies.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+INSTALL_DIR="${PWD}"
+
 sudo apt update
 
 declare -a packages=(
@@ -28,7 +30,7 @@ sudo cgset -r cpu.cfs_quota_us=100000 limitcpu1
 sudo cgcreate -a "$USER" -t "$USER" -g cpu:/limitcpu8
 sudo cgset -r cpu.cfs_quota_us=800000 limitcpu8
 
-if [[ ! -d "$PWD/duckdb-polr" ]]; then
+if [[ ! -d "${INSTALL_DIR}/duckdb-polr" ]]; then
   echo "Downloading POLAR..."
   git clone https://github.com/d-justen/duckdb-polr.git
   cd duckdb-polr
@@ -37,7 +39,7 @@ if [[ ! -d "$PWD/duckdb-polr" ]]; then
   cd ..
 fi
 
-if [[ ! -d "$PWD/ssb-dbgen" ]]; then
+if [[ ! -d "${INSTALL_DIR}/ssb-dbgen" ]]; then
   echo "Downloading SSB DBGen..."
   git clone https://github.com/eyalroz/ssb-dbgen.git
   cd ssb-dbgen
@@ -48,8 +50,8 @@ if [[ ! -d "$PWD/ssb-dbgen" ]]; then
   cd ..
   mkdir -p data/ssb
   mv ssb-dbgen/*.tbl data/ssb
-  sed -i"" -e "s|PATHVAR|`pwd`/data/ssb|" ./duckdb-polr/benchmark/ssb/init/load.sql
-  sed -i"" -e "s|PATHVAR|`pwd`/data/ssb|" ./duckdb-polr/benchmark/ssb-skew/init/load.sql
+  sed -i"" -e "s|PATHVAR|${INSTALL_DIR}/data/ssb|" ./duckdb-polr/benchmark/ssb/init/load.sql
+  sed -i"" -e "s|PATHVAR|${INSTALL_DIR}/data/ssb|" ./duckdb-polr/benchmark/ssb-skew/init/load.sql
 fi
 
 echo "Loading JOB data..."
@@ -66,9 +68,9 @@ mkdir -p data/imdb
 mkdir -p data/ssb
 mkdir -p data/ssb-skew
 
-echo "Loading benchmark data into SkinnerDB..."
-sed -i"" -e "s|PATHVAR|`pwd`|" ./experiments/util/*.sql
-sed -i"" -e "s|PATHVAR|`pwd`/data|" ./experiments/util/skinnerdb/*.sql
+echo "Preparing benchmark data for SkinnerDB..."
+sed -i"" -e "s|PATHVAR|${INSTALL_DIR}|" ./experiments/util/*.sql
+sed -i"" -e "s|PATHVAR|${INSTALL_DIR}/data|" ./experiments/util/skinnerdb/*.sql
 cat experiments/util/export-imdb.sql | duckdb-polr/build/release/duckdb duckdb-polr/duckdb_benchmark_data/imdb.duckdb
 cat experiments/util/export-ssb.sql | duckdb-polr/build/release/duckdb duckdb-polr/duckdb_benchmark_data/ssb.duckdb
 cat experiments/util/export-ssb-skew.sql | duckdb-polr/build/release/duckdb duckdb-polr/duckdb_benchmark_data/ssb-skew.duckdb
@@ -81,7 +83,7 @@ sed -i"" -e "s|\x0| |g" ./data/imdb/cast_info.tbl.dirty
 iconv -f utf-8 -t utf-8 -c ./data/imdb/cast_info.tbl.dirty > ./data/imdb/cast_info.tbl
 rm data/imdb/cast_info.tbl.dirty
 
-if [[ ! -d "$PWD/skinnerdb" ]]; then
+if [[ ! -d "${INSTALL_DIR}/skinnerdb" ]]; then
   echo "Downloading SkinnerDB..."
   git clone https://github.com/cornelldbgroup/skinnerdb.git
   gdown https://drive.google.com/uc?id=1UCXtiPvVlwzUCWxKM6ic-XqIryk4OTgE
@@ -95,7 +97,7 @@ if [[ ! -d "$PWD/skinnerdb" ]]; then
   echo -e "exec experiments/util/schema-ssb-skew.sql\nexec experiments/util/skinnerdb/load-ssb-skew.sql\nquit" | java -jar -Xmx32G -XX:+UseConcMarkSweepGC skinnerdb/jars/Skinner.jar data/skinnerssb-skew
 fi
 
-if [[ ! -d "$PWD/skinnermt" ]]; then
+if [[ ! -d "${INSTALL_DIR}/skinnermt" ]]; then
   echo "Downloading SkinnerMT"
   gdown "1CU0sJlR-GvSBKzzfJO-CyaFlM_PmN4Tb&confirm=t"
   gdown "1zr9pKMfK33IOlZ26YrvLpO1STi7Rzueu&confirm=t"
@@ -111,19 +113,13 @@ if [[ ! -d "$PWD/skinnermt" ]]; then
   cmake -DCMAKE_INSTALL_PREFIX=../install -DBUILD_EXAMPLES=1 -DBUILD_TESTS=1 .
   make all
   make install
-  cd "$PWD/skinnermt/Filter"
-  g++ -std=c++11 -lpthread -shared -fPIC -O3 jniFilter.cpp -o jniFilter.so -I"$PWD"/build/install/include/
-  cd "$PWD"
+  cd ../../skinnermt/Filter
+  g++ -std=c++11 -lpthread -shared -fPIC -O3 jniFilter.cpp -o jniFilter.so -I../../build/install/include/
+  cd ../..
   mkdir -p data/skinnermtssb
-  java -jar -Xmx32G -XX:+UseConcMarkSweepGC skinnerdb/jars/CreateDB.jar skinnerssb data/skinnermtssb
-  cd skinnermt/scripts
-  echo -e "exec $PWD/experiments/util/schema-ssb.sql\nexec $PWD/experiments/util/skinnerdb/load-ssb.sql\ncompress;\nquit" | /usr/lib/jvm/java-1.16.0-openjdk-amd64/bin/java -jar Skinner.jar "$PWD"/data/skinnermtssb
-  cd "$PWD"
   mkdir -p data/skinnermtssbskew
+  java -jar -Xmx32G -XX:+UseConcMarkSweepGC skinnerdb/jars/CreateDB.jar skinnerssb data/skinnermtssb
   java -jar -Xmx32G -XX:+UseConcMarkSweepGC skinnerdb/jars/CreateDB.jar skinnerssbskew data/skinnermtssbskew
-  cd skinnermt/scripts
-  echo -e "exec $PWD/experiments/util/schema-ssb-skew.sql\nexec $PWD/experiments/util/skinnerdb/load-ssb-skew.sql\ncompress;\nquit" | /usr/lib/jvm/java-1.16.0-openjdk-amd64/bin/java -jar Skinner.jar "$PWD"/data/skinnermtssbskew
-  cd "$PWD"
   cat <<EOT >> data/skinnermtssb/config.sdb
 PARALLEL_ALGO=DP
 NR_WARMUP=1
@@ -131,12 +127,17 @@ NR_EXECUTORS=1
 NR_BATCHES=120
 WRITE_RESULTS=false
 THREADS=8
-JNI_PATH="$PWD"/skinnermt/Filter/jniFilter.so
+JNI_PATH="${INSTALL_DIR}"/skinnermt/Filter/jniFilter.so
 EOT
   cp data/skinnermtssb/config.sdb data/skinnermtssbskew/config.sdb
+
+  cd skinnermt/scripts
+  echo -e "exec ${INSTALL_DIR}experiments/util/schema-ssb.sql\nexec ${INSTALL_DIR}/experiments/util/skinnerdb/load-ssb.sql\ncompress;\nquit" | /usr/lib/jvm/java-1.16.0-openjdk-amd64/bin/java -jar Skinner.jar "${INSTALL_DIR}"/data/skinnermtssb
+  echo -e "exec ${INSTALL_DIR}/experiments/util/schema-ssb-skew.sql\nexec ${INSTALL_DIR}/experiments/util/skinnerdb/load-ssb-skew.sql\ncompress;\nquit" | /usr/lib/jvm/java-1.16.0-openjdk-amd64/bin/java -jar Skinner.jar "${INSTALL_DIR}"/data/skinnermtssbskew
+  cd "${INSTALL_DIR}"
 fi
 
-if [[ ! -d "$PWD/venv" ]]; then
+if [[ ! -d "${INSTALL_DIR}/venv" ]]; then
   echo "Creating Python Virtual Environment"
   python3 -m venv venv
   source "venv/bin/activate"
