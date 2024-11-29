1a67f5055b75af9b3dc924b285310b0bdb8a91e2
Sebastian Baunsgaard <baunsgaard@apache.org>
2024-11-29 22:08:26 +0100

    [ExDRA] spelling mistakes in setupData

--- sigmod2021-exdra-p523/experiments/setupData.sh
+++ sigmod2021-exdra-p523/experiments/setupData.sh
@@ -2,15 +2,15 @@
 
 source parameters.sh
 
-# Syncronize the parameters file with remote
+# Synchronize the parameters file with remote
 scp -q parameters.sh $main:$remoteDir
 scp -q code/generateData.sh $main:$remoteDir/code
 
-# Syncronize the parameters file with a potentially different local location
+# Synchronize the parameters file with a potentially different local location
 cp parameters.sh ~/$remoteDir/parameters.sh
 cp code/generateData.sh ~/$remoteDir/code/generateData.sh
 
-# Only creat data on local and main machines
+# Only create data on local and main machines
 address=($main "localhost")
 
 for index in ${!address[*]}; do
