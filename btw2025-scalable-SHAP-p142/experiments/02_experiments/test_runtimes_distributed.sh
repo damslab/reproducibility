86aacb42cf91a4237b2d4e18f330dd1ae3e32f9e
louislepage <l.lepage@campus.tu-berlin.de>
2025-01-14 19:32:02 +0100

    [SCALABLE-SHAP] update dirs in distributed experiments

--- btw2025-scalable-SHAP-p142/experiments/02_experiments/test_runtimes_distributed.sh
+++ btw2025-scalable-SHAP-p142/experiments/02_experiments/test_runtimes_distributed.sh
@@ -22,24 +22,22 @@
 #
 #Runs systemds multiple times and stores resulting runtimes and sample sizes in file
 
-data_file="${1:-../data/runtimes_cluster_test.csv}"
+data_file="${1:-runtimes_distributed.csv}"
 permutations=3
 samples=100
 
-#logreg
-adult_data_sysds_str="data_dir=/user/hadoop/shap/adult/ X_bg_path=Adult_X.csv B_path=Adult_W.csv metadata_path=Adult_partitions.csv model_type=multiLogRegPredict"
-adult_data_python_str="--data-dir=/home/louislepage/data/adult/ --data-x=Adult_X.csv --model-type=multiLogReg"
+hdfs_dir="/user/hadoop/scalable-shap/10_data/"
 
-#ffn
-#ffn
-adult_ffn_data_sysds_str="data_dir=/user/hadoop/shap/adult/ X_bg_path=Adult_X.csv B_path=ffn_model.bin metadata_path=Adult_partitions.csv model_type=ffPredict"
-adult_ffn_data_python_str="--data-dir=/home/lepage/data/adult/ --data-x=Adult_X.csv --model-type=ffn"
+#logreg
+adult_data_sysds_str="data_dir=${hdfs_dir}adult/ X_bg_path=Adult_X.csv B_path=models/Adult_MLR.csv metadata_path=Adult_partitions.csv model_type=multiLogRegPredict"
+
+#fnn
+adult_ffn_data_sysds_str="data_dir=${hdfs_dir}adult/ X_bg_path=Adult_X.csv B_path=models/Adult_FNN.bin metadata_path=Adult_partitions.csv model_type=ffPredict"
 
 #svm
-census_data_sysds_str="data_dir=/user/hadoop/shap/census/ X_bg_path=census_xTrain.csv B_path=census_bias.csv metadata_path=census_dummycoding_partitions.csv model_type=l2svmPredict"
-census_data_python_str="--data-dir=/home/lepage/data/census/ --data-x=census_xTrain.csv --data-y=census_yTrain_corrected.csv --model-type=l2svm"
+census_data_sysds_str="data_dir=${hdfs_dir}census/ X_bg_path=Census_X.csv B_path=models/Census_SVM.csv metadata_path=Census_partitions.csv model_type=l2svmPredict"
 
-exp_type_array=("adult_linlogreg" "census_l2svm") #"adult_ffn"
+exp_type_array=("adult_linlogreg" "census_l2svm" "adult_fnn")
 echo "Outputfile: $data_file"
 
 echo "exp_type,instances,runtime_row,runtime_row_non_var,runtime_row_partitioned,runtime_permutation,runtime_cluster,runtime_cluster_partitioned,executors_cluster" | tee "$data_file"
@@ -50,62 +48,57 @@ for instances in $(seq 0 1000 16000); do
     #take three samples per size
     for j in {1..3}; do
 #        echo -n "${instances}," | tee -a "$data_file"
-	for num_executors in 2 4 8 ; do
-		for exp_type in "${exp_type_array[@]}"; do
-		  #exp_type="census_l2svm"
-		  if [ "$exp_type" = "adult_linlogreg" ]; then
-		      data_str=$adult_data_sysds_str
-        	      py_str=$adult_data_python_str
-        	  elif [ "$exp_type" = "adult_ffn" ]; then
-        	      data_str=$adult_ffn_data_sysds_str
-        	      py_str=$adult_ffn_data_python_str
-		  elif [ "$exp_type" = "census_l2svm" ]; then
-        	      data_str=$census_data_sysds_str
-        	      py_str=$census_data_python_str
-        	  else
-        	      echo "Exp type unknown: $exp_type"
-        	      exit 1
-        	  fi
+	    for num_executors in 2 4 8 ; do
+        for exp_type in "${exp_type_array[@]}"; do
+          if [ "$exp_type" = "adult_linlogreg" ]; then
+            data_str=$adult_data_sysds_str
+          elif [ "$exp_type" = "adult_fnn" ]; then
+            data_str=$adult_ffn_data_sysds_str
+          elif [ "$exp_type" = "census_l2svm" ]; then
+            data_str=$census_data_sysds_str
+          else
+            echo "Exp type unknown: $exp_type"
+            exit 1
+          fi
 
+          # we dont rerun all the local experiments since we already have that data
+          #by-row
+          runtime_r=''
+          #$(systemds ./shapley-permutation-experiment.dml -stats 1 -nvargs ${data_str} n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+          echo -n "${exp_type},${instances},${runtime_r}," | tee -a "$data_file"
 
-        	  #by-row
-        	  runtime_r=''
-		  #$(systemds ./shapley-permutation-experiment.dml -stats 1 -nvargs ${data_str} n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
-        	  echo -n "${exp_type},${instances},${runtime_r}," | tee -a "$data_file"
+          #by-row non var
+          runtime_r_non_var=''
+          #$(systemds ./shapley-permutation-experiment.dml -stats 1 -nvargs ${data_str} remove_non_var=1 n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+          echo -n "${runtime_r_non_var}," | tee -a "$data_file"
 
-        	  #by-row non var
-        	  runtime_r_non_var=''
-		  #$(systemds ./shapley-permutation-experiment.dml -stats 1 -nvargs ${data_str} remove_non_var=1 n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
-        	  echo -n "${runtime_r_non_var}," | tee -a "$data_file"
+          #by-row partitioned
+          runtime_r_partitioned=''
+          #runtime_r_partitioned=$(systemds ./shapley-permutation-experiment.dml -stats 1 -nvargs ${data_str} remove_non_var=0 use_partitions=1 n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row  2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+          echo -n "${runtime_r_partitioned}," | tee -a "$data_file"
 
-        	  #by-row partitioned
-        	  #runtime_r_partitioned=$(systemds ./shapley-permutation-experiment.dml -stats 1 -nvargs ${data_str} remove_non_var=0 use_partitions=1 n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row  2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
-        	  echo -n "${runtime_r_partitioned}," | tee -a "$data_file"
+          #by-permutation
+          [[ $instances -le 10000 ]] && runtime_p=''
+          #runtime_p=$(systemds ./shapley-permutation-experiment.dml -stats 1 -nvargs ${data_str} n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-permutation 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+          echo -n "${runtime_p}," | tee -a "$data_file"
+          unset runtime_p
 
-        	  #by-permutation
-        	  [[ $instances -le 10000 ]] && runtime_p=''
-		  #$(systemds ./shapley-permutation-experiment.dml -stats 1 -nvargs ${data_str} n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-permutation 2>/dev/null | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
-        	  echo -n "${runtime_p}," | tee -a "$data_file"
-        	  unset runtime_p
+          #cluster
+          runtime_cluster=$(runspark-num-executors ${num_executors} -f /home/lepage/scripts/shap/examples/shapley-permutation-experiment-spark.dml -stats 1 -nvargs remove_non_var=0 use_partitions=0 n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row ${data_str})
+          #echo "$runtime_cluster"
+          runtime_cluster=$(echo "$runtime_cluster" | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+          echo -n "${runtime_cluster}," | tee -a "$data_file"
+          unset runtime_cluster
 
-		  #cluster
-		  #data_dir=/user/hadoop/shap/census/ X_bg_path=census_xTrain.csv B_path=census_bias.csv metadata_path=census_dummycoding_partitions.csv model_type=l2svmPredict
-		  #echo "runspark-num-executors ${num_executors} -f /home/lepage/scripts/shap/examples/shapley-permutation-experiment-spark.dml -stats 1 -nvargs remove_non_var=0 use_partitions=0 n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row ${data_str}"
-		  runtime_cluster=$(runspark-num-executors ${num_executors} -f /home/lepage/scripts/shap/examples/shapley-permutation-experiment-spark.dml -stats 1 -nvargs remove_non_var=0 use_partitions=0 n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row ${data_str})
-		  #echo "$runtime_cluster"
-		  runtime_cluster=$(echo "$runtime_cluster" | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
-        	  echo -n "${runtime_cluster}," | tee -a "$data_file"
-        	  unset runtime_cluster
+          runtime_cluster_partitioned=$(runspark-num-executors ${num_executors} -f /home/lepage/scripts/shap/examples/shapley-permutation-experiment-spark.dml -stats 1 -nvargs remove_non_var=0 use_partitions=1 n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row ${data_str})
+          #echo "$runtime_cluster_partitioned"
+          runtime_cluster_partitioned=$(echo "$runtime_cluster_partitioned" | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
+          echo -n "${runtime_cluster_partitioned},${num_executors}" | tee -a "$data_file"
+          unset runtime_cluster_partitioned
 
-		  runtime_cluster_partitioned=$(runspark-num-executors ${num_executors} -f /home/lepage/scripts/shap/examples/shapley-permutation-experiment-spark.dml -stats 1 -nvargs remove_non_var=0 use_partitions=1 n_permutations=${permutations} integration_samples=${samples} rows_to_explain=${instances} write_to_file=0 execution_policy=by-row ${data_str})
-		  #echo "$runtime_cluster_partitioned"
-		  runtime_cluster_partitioned=$(echo "$runtime_cluster_partitioned" | grep "Total elapsed time" | awk '{print $4}' | tr \, \.)
-        	  echo -n "${runtime_cluster_partitioned},${num_executors}" | tee -a "$data_file"
-        	  unset runtime_cluster_partitioned
-
-        	  #newline
-        	  echo "" | tee -a "$data_file"
-       		done
-	done
+          #newline
+          echo "" | tee -a "$data_file"
+        done
+	    done
     done
 done
